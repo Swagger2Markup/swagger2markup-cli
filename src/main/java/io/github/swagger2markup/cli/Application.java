5965f36187f6a11474cea686e3fff1944e1a1eb8
Robert Winkler <robert.winkler@telekom.de>
2016-04-04 11:14:18 +0200

    Input file can be a URL and a local file now Output path can be a file and a directory now

--- src/main/java/io/github/swagger2markup/cli/Application.java
+++ src/main/java/io/github/swagger2markup/cli/Application.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Robert Winkler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.swagger2markup.cli;
 
 import io.airlift.airline.*;
@@ -9,6 +24,7 @@ import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
+import java.net.URI;
 import java.nio.file.Paths;
 
 @Command(name = "convert", description = "Converts a Swagger JSON or YAML file into Markup documents.")
@@ -17,13 +33,13 @@ public class Application implements Runnable{
     @Inject
     public HelpOption helpOption;
 
-    @Option(name = {"-i", "--inputFile"}, required = true, description = "Input file")
+    @Option(name = {"-i", "--input"}, required = true, description = "Input. Can either be a URL or a file.")
     public String inputFile;
 
-    @Option(name = {"-o", "--outputPath"}, required = true, description = "Output path")
+    @Option(name = {"-o", "--output"}, required = true, description = "Output path. Can either be a directory or a file.")
     public String outputPath;
 
-    @Option(name = {"-c", "--configFile"}, description = "Config file")
+    @Option(name = {"-c", "--config"}, description = "Config file.")
     public String configFile;
 
     public static void main(String[] args) {
@@ -45,12 +61,12 @@ public class Application implements Runnable{
                 Configuration config = configs.properties(configFile);
                 swagger2MarkupConfig = new Swagger2MarkupConfigBuilder(config).build();
             }
-            Swagger2MarkupConverter.Builder converterBuilder = Swagger2MarkupConverter.from(Paths.get(inputFile));
+            Swagger2MarkupConverter.Builder converterBuilder = Swagger2MarkupConverter.from(URI.create(inputFile));
             if(swagger2MarkupConfig != null){
                 converterBuilder.withConfig(swagger2MarkupConfig);
             }
             converterBuilder.build()
-                    .toFolder(Paths.get(outputPath));
+                    .toPath(Paths.get(this.outputPath));
         } catch (Exception e) {
             e.printStackTrace();
         }
