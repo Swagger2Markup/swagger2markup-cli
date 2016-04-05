1bd548b3211b277b807906a74c9baa51f4a11ed2
Robert Winkler <robert.winkler@telekom.de>
2016-04-05 09:25:25 +0200

    Renamed output options

--- src/main/java/io/github/swagger2markup/cli/Application.java
+++ src/main/java/io/github/swagger2markup/cli/Application.java
@@ -21,7 +21,10 @@ import io.github.swagger2markup.Swagger2MarkupConverter;
 import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.net.URI;
@@ -30,14 +33,19 @@ import java.nio.file.Paths;
 @Command(name = "convert", description = "Converts a Swagger JSON or YAML file into Markup documents.")
 public class Application implements Runnable{
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     @Inject
     public HelpOption helpOption;
 
-    @Option(name = {"-i", "--input"}, required = true, description = "Input. Can either be a URL or a file.")
-    public String inputFile;
+    @Option(name = {"-i", "--input"}, required = true, description = "Swagger source. Can either be a URL or a file path.")
+    public String input;
 
-    @Option(name = {"-o", "--output"}, required = true, description = "Output path. Can either be a directory or a file.")
-    public String outputPath;
+    @Option(name = {"-d", "--outputDir"}, description = "Output directory. Converts the Swagger specification into multiple files.")
+    public String outputDir;
+
+    @Option(name = {"-f", "--outputFile"}, description = "Output file. Converts the Swagger specification into one file.")
+    public String outputFile;
 
     @Option(name = {"-c", "--config"}, description = "Config file.")
     public String configFile;
@@ -54,21 +62,30 @@ public class Application implements Runnable{
     }
 
     public void run() {
-        try {
-            Swagger2MarkupConfig swagger2MarkupConfig = null;
-            if(StringUtils.isNotBlank(configFile)) {
-                Configurations configs = new Configurations();
-                Configuration config = configs.properties(configFile);
-                swagger2MarkupConfig = new Swagger2MarkupConfigBuilder(config).build();
+
+        Swagger2MarkupConfig swagger2MarkupConfig = null;
+        if(StringUtils.isNotBlank(configFile)) {
+            Configurations configs = new Configurations();
+            Configuration config;
+            try {
+                config = configs.properties(configFile);
+            } catch (ConfigurationException e) {
+                throw new IllegalArgumentException("Failed to read configFile", e);
             }
-            Swagger2MarkupConverter.Builder converterBuilder = Swagger2MarkupConverter.from(URI.create(inputFile));
-            if(swagger2MarkupConfig != null){
-                converterBuilder.withConfig(swagger2MarkupConfig);
-            }
-            converterBuilder.build()
-                    .toPath(Paths.get(this.outputPath));
-        } catch (Exception e) {
-            e.printStackTrace();
+            swagger2MarkupConfig = new Swagger2MarkupConfigBuilder(config).build();
+        }
+        Swagger2MarkupConverter.Builder converterBuilder = Swagger2MarkupConverter.from(URI.create(input));
+        if(swagger2MarkupConfig != null){
+            converterBuilder.withConfig(swagger2MarkupConfig);
+        }
+        Swagger2MarkupConverter converter = converterBuilder.build();
+
+        if(StringUtils.isNotBlank(outputFile)){
+            converter.toFile(Paths.get(outputFile).toAbsolutePath());
+        }else if (StringUtils.isNotBlank(outputDir)){
+            converter.toFolder(Paths.get(outputDir).toAbsolutePath());
+        }else {
+            throw new IllegalArgumentException("Either outputFile or outputDir option must be used");
         }
     }
 
